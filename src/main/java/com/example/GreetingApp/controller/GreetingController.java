69fc331ca3d454c15ef2beaee21dd5c033235e2f
Akash Pandey <akashpande027@gmail.com>
2025-03-03 18:19:58 +0530

    Hello msg

--- src/main/java/com/example/GreetingApp/controller/GreetingController.java
+++ src/main/java/com/example/GreetingApp/controller/GreetingController.java
@@ -1,4 +1,4 @@
-package com.example.GreetingApp.Controller;
+package com.example.GreetingApp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
